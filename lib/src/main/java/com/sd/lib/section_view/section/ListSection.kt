074de95ce400792215a860621dfffcefac5cd637
zjun <565061763@qq.com>
2021-07-21 19:28:16 +0800

    ListSection

--- lib/src/main/java/com/sd/lib/section_view/section/ListSection.kt
+++ lib/src/main/java/com/sd/lib/section_view/section/ListSection.kt
@@ -8,6 +8,10 @@ import com.sd.lib.section_view.section.BaseSection
 abstract class ListSection : BaseSection() {
     var recyclerView: RecyclerView? = null
 
+    override fun getLayoutId(): Int {
+        return R.layout.lib_section_view_view_list_section
+    }
+
     override fun initSectionView(view: View) {
         recyclerView = view.findViewById(R.id.rv_item)
     }
