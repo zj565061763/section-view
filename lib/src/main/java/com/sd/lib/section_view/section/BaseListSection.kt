87bb16110af24780db4ae346648d4453de5bf15d
zjun <565061763@qq.com>
2021-07-22 10:08:08 +0800

    ListSection

--- lib/src/main/java/com/sd/lib/section_view/section/BaseListSection.kt
+++ lib/src/main/java/com/sd/lib/section_view/section/BaseListSection.kt
@@ -1,17 +1,65 @@
 package com.sd.lib.section_view.section
 
+import android.graphics.Color
 import android.view.View
+import androidx.annotation.CallSuper
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.sd.lib.section_view.R
+import com.sd.lib.section_view.model.Brightness
+import com.sd.lib.section_view.model.IBrightnessReceiver
 
-abstract class BaseListSection<T> : BaseSection<T>() {
+abstract class BaseListSection<T> : BaseSection<T> {
+    val spanCount: Int
     var recyclerView: RecyclerView? = null
+
+    constructor(spanCount: Int) : super() {
+        require(spanCount >= 1)
+        this.spanCount = spanCount
+    }
 
     override fun getLayoutId(): Int {
         return R.layout.lib_section_view_view_list_section
     }
 
+    @CallSuper
     override fun initSectionView(view: View) {
         recyclerView = view.findViewById(R.id.rv_item)
+        recyclerView?.apply {
+            if (spanCount == 1) {
+                this.layoutManager = LinearLayoutManager(view.context)
+            } else {
+                this.layoutManager = GridLayoutManager(view.context, spanCount)
+            }
+        }
     }
+
+    override fun onUpdateBrightness(view: View, brightness: Brightness) {
+        super.onUpdateBrightness(view, brightness)
+        when (brightness) {
+            Brightness.Light -> {
+                view.apply {
+                    this.setBackgroundColor(Color.WHITE)
+                }
+            }
+            Brightness.Dark -> {
+                view.apply {
+                    this.setBackgroundColor(Color.BLACK)
+                }
+            }
+        }
+        recyclerView?.adapter?.let { adapter ->
+            if (adapter is IBrightnessReceiver) {
+                adapter.setBrightness(brightness)
+            }
+        }
+    }
+
+    @CallSuper
+    override fun onBindData(view: View, data: T) {
+        recyclerView?.adapter = createAdapter(data)
+    }
+
+    protected abstract fun createAdapter(data: T): RecyclerView.Adapter<*>
 }
---
+++ lib/src/main/java/com/sd/lib/section_view/section/ListSection.kt
@@ -0,0 +1,55 @@
+package com.sd.lib.section_view.section
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.CallSuper
+import androidx.recyclerview.widget.RecyclerView
+import com.sd.lib.section_view.model.Brightness
+import com.sd.lib.section_view.model.IBrightnessReceiver
+import java.util.*
+
+abstract class ListSection<VH : RecyclerView.ViewHolder> : BaseListSection<List<Any>> {
+    private val _mapViewHolder = WeakHashMap<RecyclerView.ViewHolder, String>()
+
+    constructor(spanCount: Int) : super(spanCount) {
+    }
+
+    @CallSuper
+    override fun onUpdateBrightness(view: View, brightness: Brightness) {
+        super.onUpdateBrightness(view, brightness)
+        _mapViewHolder.keys.forEach { viewHolder ->
+            if (viewHolder is IBrightnessReceiver) {
+                viewHolder.setBrightness(brightness)
+            }
+        }
+    }
+
+    override fun createAdapter(data: List<Any>): RecyclerView.Adapter<VH> {
+        return object : RecyclerView.Adapter<VH>() {
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+                val viewHolder = this@ListSection.onCreateViewHolder(parent, viewType)
+                if (viewHolder is IBrightnessReceiver) {
+                    viewHolder.setBrightness(getBrightness())
+                }
+                return viewHolder.also {
+                    _mapViewHolder.put(it, "")
+                }
+            }
+
+            override fun onBindViewHolder(holder: VH, position: Int) {
+                val model = data.getOrNull(position)
+                if (model != null) {
+                    this@ListSection.onBindViewHolder(holder, position, model)
+                }
+            }
+
+            override fun getItemCount(): Int {
+                return data.size
+            }
+        }
+    }
+
+    protected abstract fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH
+
+    protected abstract fun onBindViewHolder(holder: VH, position: Int, model: Any)
+}
